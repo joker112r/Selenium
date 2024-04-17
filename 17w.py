c982082d4329599f185c381405859286a2c26197
joker112r <joker112r@gmail.com>
2024-04-17 14:53:53 +0300

    112223

--- 17w.py
+++ 17w.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import math
 
 browser = webdriver.Chrome()
 browser.get("http://suninjuly.github.io/explicit_wait2.html")
