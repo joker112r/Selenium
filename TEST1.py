cdbd2c837889f0e92adeeed078bcccfd3bf7fd28
joker112r <163987346+joker112r@users.noreply.github.com>
2024-04-23 03:14:55 +0300

    Update TEST1.py

--- TEST1.py
+++ TEST1.py
@@ -5,64 +5,64 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-# Настройка драйвера Selenium
 driver = webdriver.Chrome()
 driver.maximize_window()
-
-# Переход на сайт
 driver.get("https://easysmarthub.ru/shop")
 
-# Добавление первого товара в корзину
-product_1 = driver.find_element(By.CSS_SELECTOR, ".product-card__button")
+product_1 = driver.find_element(By.CSS_SELECTOR, '[data-product_id="1008"]')
 product_1.click()
 
-# Нажатие на кнопку "Продолжить покупки"
-continue_shopping_button = driver.find_element(By.CSS_SELECTOR, ".btn-continue-shopping")
+continue_shopping_button = driver.find_element(By.CSS_SELECTOR, '[class="button wc-forward wp-element-button"]')
 continue_shopping_button.click()
 
-# Добавление второго товара в корзину
-product_2 = driver.find_element(By.CSS_SELECTOR, ".product-card__button")
+product_2 = driver.find_element(By.CSS_SELECTOR, '[data-product_id="1009"]')
 product_2.click()
 
-# Нажатие на кнопку "Продолжить покупки"
-continue_shopping_button = driver.find_element(By.CSS_SELECTOR, ".btn-continue-shopping")
+continue_shopping_button = driver.find_element(By.CSS_SELECTOR, '[class="button wc-forward wp-element-button"]')
 continue_shopping_button.click()
 
-# Добавление третьего товара в корзину
-product_3 = driver.find_element(By.CSS_SELECTOR, ".product-card__button")
+product_3 = driver.find_element(By.CSS_SELECTOR, '[data-product_id="1011"]')
 product_3.click()
 
-# Переход к оформлению заказа
-checkout_button = driver.find_element(By.CSS_SELECTOR, ".btn-checkout")
+checkout_button = driver.find_element(By.CSS_SELECTOR,'[href="https://easysmarthub.ru/checkout/"]')
 checkout_button.click()
 
 # Заполнение полей оформления заказа
-email_field = driver.find_element(By.ID, "email")
+email_field = driver.find_element(By.ID,'billing_email')
 email_field.send_keys("joker112r@gmail.com")
 
-name_field = driver.find_element(By.ID, "name")
+name_field = driver.find_element(By.ID,'billing_first_name')
 name_field.send_keys("Robert")
 
 second_name_field = driver.find_element(By.ID, "billing_last_name")
 second_name_field.send_keys("Razuvaev")
 
-phone_field = driver.find_element(By.ID, "phone")
+phone_field = driver.find_element(By.ID,'billing_phone')
 phone_field.send_keys("+79990000000")
+time.sleep(5)
 
-# Переход к оплате
-submit_order_button = driver.find_element(By.CSS_SELECTOR, ".btn-submit-order")
+submit_order_button = driver.find_element(By.NAME,'woocommerce_checkout_place_order')
 submit_order_button.click()
+driver.implicitly_wait(7)
 
-# Заполнение полей оплаты
-card_number_field = driver.find_element(By.ID, "card-number")
+card_number_field = driver.find_element(By.CSS_SELECTOR, '[autocomplete="cc-number"]')
 card_number_field.send_keys("5555555555554477")
 
-card_expiration_date_field = driver.find_element(By.ID, "card-expiration-date")
-card_expiration_date_field.send_keys("04/28")
+card_expiration_date_field = driver.find_element(By.CSS_SELECTOR, '[autocomplete="cc-exp-month"]')
+card_expiration_date_field.send_keys("04")
 
-card_cvv_field = driver.find_element(By.ID, "card-cvv")
+card_expiration_date_field_2 = driver.find_element(By.CSS_SELECTOR, '[autocomplete="cc-exp-year"]')
+card_expiration_date_field_2.send_keys("28")
+
+card_cvv_field = driver.find_element(By.CSS_SELECTOR, '[placeholder="CVC"]')
 card_cvv_field.send_keys("333")
-
+chek_1 = driver.find_element(By.ID, 'send-email-invoice')
+chek_1.click()
+time.sleep(5)
+emlg = driver.find_element(By.CSS_SELECTOR, '[data-qa-bankcard-field-name="invoice-email"]')
+emlg.send_keys('joker112r@gmail.com')
+subm = driver.find_element(By.CSS_SELECTOR, '[type="submit"]')
+subm.click()
 # Проверка успешности теста
 confirmation_message = WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.CSS_SELECTOR, ".confirmation-message"))
@@ -71,3 +71,4 @@ confirmation_message = WebDriverWait(driver, 10).until(
 assert "Ваш заказ успешно оформлен" in confirmation_message.text
 
 driver.quit()
+
